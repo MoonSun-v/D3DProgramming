c7b8ae6a317d6b61b2b00431ab637a815e400fac
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2025-09-02 11:16:17 +0900

    [Feat] Props 추가 및 프로젝트 설정

---
+++ D3DProgramming/D3DProgramming/TestApp.cpp
@@ -0,0 +1,143 @@
+#include "TestApp.h"
+#include "../Common/Helper.h"
+
+#include <directxtk/simplemath.h>
+#include <dxgidebug.h>
+#include <dxgi1_3.h>
+#include <wrl/client.h>
+
+#pragma comment (lib, "d3d11.lib")
+#pragma comment(lib,"dxgi.lib")
+
+using namespace DirectX::SimpleMath;
+using Microsoft::WRL::ComPtr;
+
+#define USE_FLIPMODE 1			//경고 메세지를 없애려면 Flip 모드를 사용한다.
+
+TestApp::TestApp(HINSTANCE hInstance) :GameApp(hInstance)
+{
+
+}
+
+TestApp::~TestApp()
+{
+	UninitD3D();
+}
+
+bool TestApp::Initialize(UINT Width, UINT Height)
+{
+	__super::Initialize(Width, Height);
+
+	if (!InitD3D())
+		return false;
+
+	return true;
+}
+
+void TestApp::Update()
+{
+
+}
+
+void TestApp::Render()
+{
+#if USE_FLIPMODE==1
+	// Flip모드에서는 매프레임 설정 필요
+	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, NULL);
+#endif
+	Color color(0.0f, 0.5f, 0.5f, 1.0f);
+	// 화면 칠하기.
+	m_pDeviceContext->ClearRenderTargetView(m_pRenderTargetView, color);
+
+	// 스왑체인 교체.
+	m_pSwapChain->Present(0, 0);
+}
+
+
+bool TestApp::InitD3D()
+{
+	HRESULT hr = 0;
+
+	// 1. D3D11 Device,DeviceContext 생성
+	UINT creationFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+#ifdef _DEBUG
+	creationFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+	// 그래픽 카드 하드웨어의 스펙으로 호환되는 가장 높은 DirectX 기능레벨로 생성하여 드라이버가 작동 한다.
+	// 인터페이스는 Direc3D11 이지만 GPU드라이버는 D3D12 드라이버가 작동할수도 있다.
+	D3D_FEATURE_LEVEL featureLevels[] = { // index 0부터 순서대로 시도한다.
+		D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0,D3D_FEATURE_LEVEL_11_1,D3D_FEATURE_LEVEL_11_0
+	};
+	D3D_FEATURE_LEVEL actualFeatureLevel; // 최종 피처 레벨을 저장할 변수
+
+	HR_T(D3D11CreateDevice(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		0,
+		creationFlags,
+		featureLevels,
+		ARRAYSIZE(featureLevels),
+		D3D11_SDK_VERSION,
+		&m_pDevice,
+		&actualFeatureLevel,
+		&m_pDeviceContext
+	));
+
+	// 2. 스왑체인 생성을 위한 DXGI Factory 생성
+	UINT dxgiFactoryFlags = 0;
+#ifdef _DEBUG
+	dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+#endif
+
+	ComPtr<IDXGIFactory2> pFactory;
+	HR_T(CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&pFactory)));
+
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+#if USE_FLIPMODE==1
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+#else
+	swapChainDesc.BufferCount = 1;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+#endif
+	swapChainDesc.Width = m_ClientWidth;
+	swapChainDesc.Height = m_ClientHeight;
+	// 하나의 픽셀이 채널 RGBA 각 8비트 형식으로 표현되며 
+	// Unsigned Normalized Integer 8비트 정수(0~255)단계를 부동소수점으로 정규화한 0.0~1.0으로 매핑하여 표현한다.
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT; // 스왑 체인의 백 버퍼가 렌더링 파이프라인의 최종 출력 대상으로 사용
+	swapChainDesc.SampleDesc.Count = 1;  // 멀티샘플링 사용 안함
+	swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_IGNORE; // Recommended for flip models
+	swapChainDesc.Stereo = FALSE;  // 스테레오 3D 렌더링을 비활성화
+	swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; // 전체 화면 전환을 허용
+	swapChainDesc.Scaling = DXGI_SCALING_NONE; //  창의 크기와 백 버퍼의 크기가 다를 때. 백버퍼 크기에 맞게 스케일링 하지 않는다.
+
+	HR_T(pFactory->CreateSwapChainForHwnd(
+		m_pDevice,
+		m_hWnd,
+		&swapChainDesc,
+		nullptr,
+		nullptr,
+		&m_pSwapChain
+	));
+
+	// 3. 렌더타겟 뷰 생성.  렌더 타겟 뷰는 "여기다가 그림을 그려라"라고 GPU에게 알려주는 역할을 하는 객체.
+	// 텍스처와 영구적 연결되는 객체이다. 
+	ComPtr<ID3D11Texture2D> pBackBufferTexture;
+	HR_T(m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBufferTexture));
+	HR_T(m_pDevice->CreateRenderTargetView(pBackBufferTexture.Get(), nullptr, &m_pRenderTargetView));
+
+#if !USE_FLIPMODE
+	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, nullptr);
+#endif
+
+	return true;
+}
+
+void TestApp::UninitD3D()
+{
+	SAFE_RELEASE(m_pRenderTargetView);
+	SAFE_RELEASE(m_pDeviceContext);
+	SAFE_RELEASE(m_pSwapChain);
+	SAFE_RELEASE(m_pDevice);
+}
